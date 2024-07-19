6f845a697096ff138d4e4cad68c21fccecaf8fe3
Dieuleveut-Ngele <dieuleveutngele@gmail.com>
2024-07-19 15:27:43 +0100

    feat: add Lading page feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ShareImageListComponent } from './share-image-list/share-image-list.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 export const routes: Routes = [
-    { path: 'shareimages', component: ShareImageListComponent }
+    { path: 'shareimages', component: ShareImageListComponent },
+    { path: '', component: LandingPageComponent }
 ];
