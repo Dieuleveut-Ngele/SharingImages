57b5e42f00d5199f24aee902d397545ce07f65b1
Dieuleveut-Ngele <dieuleveutngele@gmail.com>
2024-08-02 13:08:01 +0100

    feat: added an observable to handle increasing values ​​or numbers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { RouterOutlet } from '@angular/router';
+import { filter, interval, map, Observable, pipe, tap } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 
 @Component({
@@ -8,12 +10,34 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [
     HeaderComponent,
-    RouterOutlet
+    RouterOutlet,
+    AsyncPipe
     
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent{
+export class AppComponent implements OnInit {
+
+  interval$!: Observable<string>
+
+  ngOnInit() {
+    this.interval$ = interval(1000).pipe(
+      filter(value => value % 3 === 0),
+      map(value => value % 2 === 0 ?
+          `Je suis ${value} et je suis pair` :
+          `Je suis ${value} et je suis impair`
+      ),
+      tap(text => this.logger(text))
+    );  
+  }
+  logger(text: string): void {
+    console.log(`Log: ${text}`);
+  }
 }
+
+
+  
+ 
+
 
